8128db437b951eb43a5c19b5f0625c778bded5f3
pratik-git10 <93414233+pratik-git10@users.noreply.github.com>
2023-08-17 14:34:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+console.log("Welcome To Spotify");
+// initialize the variables
+let songIndex =0
+let audioElement = new Audio('tera.mp3'); 
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let songItem =Array.from(document.getElementsByClassName('songItem'));
+
+let songs =[
+    {songName: "Tera fitoor", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Tera fitoor", filePath: "tera.mp3", coverPath:"covers/2.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/2.mp3", coverPath:"covers/3.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/3.mp3", coverPath:"covers/4.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/4.mp3", coverPath:"covers/5.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/5.mp3", coverPath:"covers/6.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/6.mp3", coverPath:"covers/7.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/7.mp3", coverPath:"covers/8.jpg"},
+    {songName: "Tera fitoor", filePath: "songs/8.mp3", coverPath:"covers/9.jpg"}, 
+    {songName: "Tera fitoor", filePath: "songs/9.mp3", coverPath:"covers/10.jpg"},
+    
+]
+songItem.forEach((element, i)=>{
+
+    element.getElementsByTagName('img')[0].src = songs[i].coverPath;
+    element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
+
+})
+
+
+// audioElement.play();
+
+// handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity= 1;
+    }
+      else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity= 0;
+
+      }
+})
+
+// Listen to Event
+audioElement.addEventListener('timeupdate', ()=>{
+    
+    // update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+   
+    myProgressBar.value = progress;
+
+} )
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
+
+})
+const makeAllPlays = ()=>{
+    
+    Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+})
+           
+}
+Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        console.console.log(e.target);
+        makeAllPlays();
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src="songs/1.mp3";
+        audioElement.currentTime =0;
+        audioElement.play;
+           
+    })
+
+})
